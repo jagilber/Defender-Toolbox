feecad8f0971d2c21b66f9d46bd90bddf9f29759
jagilber <jagilber@microsoft.com>
2024-09-11 22:21:52 -0400

    init script to read MpCmdRun*.log files and output list of powershell objects

---
+++ Read-MpCmdRunLog.ps1
@@ -0,0 +1,322 @@
+<#
+.SYNOPSIS
+Reads the MpCmdRun*.log files and returns the results as an array of PowerShell objects.
+
+.DESCRIPTION
+Reads the MpCmdRun*.log files and returns the results as an array of PowerShell objects.
+Results are also stored in the global variable $global:mpCmdRunLogResults.
+To troubleshoot, use -Verbose to see additional information.
+
+.NOTES
+File Name      : Read-mpCmdRunLog.ps1
+version        : 0.0
+
+.EXAMPLE
+C:\'Program Files'\'Windows Defender'\MpCmdRun.exe -GetFiles
+copy 'C:\ProgramData\Microsoft\Windows Defender\Support\MpSupportFiles.cab'
+md $pwd\MpSupportFiles
+expand -R -I $pwd\MpSupportFiles.cab -F:* $pwd\MpSupportFiles
+
+To generate the mpCmdRun.log file
+
+.EXAMPLE
+.\Read-mpCmdRunLog.ps1 -logFilePath $pwd\MpSupportFiles\mpCmdRun.log
+
+Reads the mpCmdRun.log file and returns the results as a PowerShell object.
+
+.PARAMETER logFilePath
+The path to the mpCmdRun.log file.
+
+.PARAMETER errorsOnly
+Show only errors.
+
+.PARAMETER quiet
+Do not show any output.
+
+.PARAMETER verbose
+Show additional information for troubleshooting.
+#>
+[cmdletbinding()]
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$logFilePath,
+  [switch]$errorsOnly,
+  [switch]$quiet
+)
+
+$global:mpCmdRunLogResults = [ordered]@{}
+$recordSeparator = '-------------------------------------------------------------------------------------'
+$global:records = [collections.arrayList]::new()
+$scriptName = "$psscriptroot\$($MyInvocation.MyCommand.Name)"
+$recordIdentifier = 'MpCmdRun: '
+$startOfRecord = "$($recordIdentifier)Command Line:"
+$endOfRecord = "$($recordIdentifier)End Time:"
+$mpCmdRunExe = 'MpCmdRun.exe'
+
+function main() {
+  try {
+    if (!(Test-Path $logFilePath)) {
+      Get-Help $scriptName -Examples
+      Write-Host "File not found: $logFilePath"
+      return $null
+    }
+
+    $global:mpCmdRunLogResults = Read-Records $logFilePath
+
+    $eventTypes = $global:mpCmdRunLogResults.Command | Group-Object | Sort-Object | Select-Object Count,Name
+    Write-Host "Event Types: $($eventTypes | Out-String)" -ForegroundColor Green
+
+    $errorEventTypes = $global:mpCmdRunLogResults.Where({$psitem.Level -ine 'information'}).Command | Group-Object | Sort-Object | Select-Object Count,Name
+    Write-Host "Event Types with Errors: $($errorEventTypes | out-string)" -ForegroundColor Red
+
+    return $global:mpCmdRunLogResults
+  }
+  catch {
+    write-verbose "variables:$((get-variable -scope local).value | convertto-json -WarningAction SilentlyContinue -depth 2)"
+    write-host "exception::$($psitem.Exception.Message)`r`n$($psitem.scriptStackTrace)" -ForegroundColor Red
+    return $null
+  }
+}
+
+function Find-RecordMatches([collections.arrayList]$record, [string]$pattern, [bool]$ignoreCase = $true, [Text.RegularExpressions.RegexOptions]$regexOptions = [Text.RegularExpressions.RegexOptions]::None) {
+  Write-Verbose "Find-RecordMatches([string]$record,[string]$pattern, [bool]$ignoreCase, [Text.RegularExpressions.RegexOptions]$regexOptions)"
+  $line = [string]::Join("`n", $record.ToArray())
+  return Find-Matches -line $line -pattern $pattern -ignoreCase $ignoreCase -regexOptions $regexOptions
+}
+
+function Find-Matches([string]$line, [string]$pattern, [bool]$ignoreCase = $true, [Text.RegularExpressions.RegexOptions]$regexOptions = [Text.RegularExpressions.RegexOptions]::None) {
+  Write-Verbose "Find-Matches([string]$line, [string]$pattern, [bool]$ignoreCase, [Text.RegularExpressions.RegexOptions]$regexOptions)"
+  if (!$line) {
+    Write-Error "Regex-Match: No line provided"
+    throw
+    return $null
+  }
+
+  if (!$pattern) {
+    Write-Error "Regex-Match: No pattern provided"
+    throw
+    return $null
+  }
+
+  if ($ignoreCase) {
+    $regexOptions = $regexOptions -bor [Text.RegularExpressions.RegexOptions]::IgnoreCase
+  }
+
+  Write-Verbose "Regex-Match: [regex]::Match($line, $pattern, $regexOptions)"
+  $regexMatchCollection = [regex]::Matches($line, $pattern, $regexOptions)
+
+  if ($regexMatchCollection.Count -eq 0) {
+    Write-Verbose "Regex-Match: No match found for pattern: $pattern"
+    return $null
+  }
+  elseif ($regexMatchCollection.Count -gt 1) {
+    Write-Verbose "Regex-Match: Multiple matches found for pattern: $pattern returning collection"
+    return $regexMatchCollection
+  }
+
+  foreach ($regexMatch in $regexMatchCollection) {
+    if ($regexMatch.Success) {
+      if ($regexMatch.Groups.Count -eq 1 -and [string]::IsNullOrEmpty($regexMatch.Groups[0].Value)) {
+        Write-Verbose "Regex-Match: No groups found. line: $line pattern: $pattern"
+        return $null
+      }
+
+      # create key value pair
+      $groups = @{ }
+      foreach ($group in $regexMatch.Groups) {
+        Write-Verbose "Regex-Match: Group: $($group.Name) = $($group.Value)"
+        [void]$groups.Add($group.Name.Trim(), $group.Value.Trim())
+      }
+
+      return $groups
+    }
+  }
+
+  Write-Verbose "Regex-Match: No match found for pattern: $pattern"
+  return $null
+}
+
+function Format-Timestamp([string]$timestamp, [string]$timeFormat = "yyyy-MM-ddTHH:mm:ss.fffzzz") {
+  return [dateTimeOffset]::Parse($timestamp).ToString($timeFormat)
+}
+
+function Get-Commands([string]$commandLine, [collections.specialized.orderedDictionary]$record) {
+  Write-Verbose "Get-Commands([string]$commandLine)"
+  $commandOptions = [collections.Arraylist]::new()
+  $commandString = (Find-Matches -line $commandLine -pattern "$mpCmdRunExe`"?(?<commandString>.+)")
+
+  if (!$commandString) {
+    return $commandOptions
+  }
+
+  $record.CommandLine = $commandLine.Replace('Command Line: ', '').Trim()
+  $commands = $commandString['commandString'].Split(' ', [StringSplitOptions]::RemoveEmptyEntries)
+  $optionName = ''
+
+  foreach ($command in $commands) {
+    if ($command -eq $commands[0]) {
+      $record.Command = $command.Replace('-', '').Trim()
+      continue
+    }
+    if ($command.StartsWith('-')) {
+      $optionName = $command.Replace('-', '').Trim()
+      $record.CommandOptions += @{$optionName = "" }
+    }
+    else {
+      $record.CommandOptions[$optionName] += $command.Trim()
+    }
+  }
+}
+
+function New-Record() {
+  $record = [ordered]@{
+    'StartTime'      = ''
+    'EndTime'        = ''
+    'Level'          = 'Information' # Information, Warning, Error
+    'CommandLine'    = ''
+    'Command'        = ''
+    'CommandOptions' = [ordered]@{}
+    'CommandReturn'  = ''
+    'Details'        = @()
+  }
+  return $record
+}
+
+function Read-PropertyNameValue(
+  [collections.arrayList]$record,
+  [string]$propertyName,
+  [string]$propertyNamePrefix = '.+?',
+  [string]$separator = ':',
+  [switch]$all
+) {
+  Write-Verbose "Read-PropertyNameValue([collections.arrayList]$($record.Count), [string]$propertyName, [string]$propertyNamePrefix, [string]$separator)"
+  $kvpCollection = [collections.arrayList]::new()
+  $pattern = "$($propertyNamePrefix)(?<propertyName>$($propertyName))\s*?$separator\s*?(?<propertyValue>.*)"
+
+  foreach ($line in $record) {
+    $regexMatch = Find-Matches -line $line -pattern $pattern
+    if ($regexMatch) {
+      $propertyName = $regexMatch['propertyName']
+      $propertyValue = $regexMatch['propertyValue'].Trim()
+      Write-Verbose "Property name value found: $propertyName = $propertyValue"
+      $kvp = @{
+        'PropertyName'  = $propertyName
+        'PropertyValue' = $propertyValue
+      }
+      if ($all) {
+        [void]$kvpCollection.Add($kvp)
+      }
+      else {
+        return $kvp
+      }
+    }
+  }
+
+  if ($all) {
+    return $kvpCollection
+  }
+  Write-Warning "Property name value not found: $propertyName"
+  return $null
+}
+
+function Read-Record([collections.arrayList]$record, [int]$index) {
+  Write-Verbose "Read-Record([collections.arrayList]$($record.Count), [int]$index)"
+  #$newRecord = Read-RecordMetaData -record $record -newRecord (New-Record)
+  $newRecord = New-Record
+  $returnCode = Find-RecordMatches -record $record -pattern "MpCmdRun.exe: hr = (?<returnCode>.+)"
+  if ($returnCode) {
+    $newRecord.CommandReturn = $returnCode['returnCode']
+  }
+
+  if (Find-RecordMatches -record $record -pattern "Error|Failed|0x[0-9A-Fa-f]8") {
+    $newRecord.Level = 'Error'
+  }
+  elseif (Find-RecordMatches -record $record -pattern "Warning") {
+    $newRecord.Level = 'Warning'
+  }
+  else {
+    $newRecord.Level = 'Information'
+  }
+
+  # Read first Line for Command Line
+  $recordIndex = 0
+  $line = $record[$recordIndex++].Trim()
+  Get-Commands -commandLine $line -record $newRecord
+  # Read second line for Start Time
+  $line = $record[$recordIndex++].Replace('Start Time: ', '').Trim()
+  $newRecord.StartTime = Format-Timestamp -timestamp $line
+
+  for ($recordIndex; $recordIndex -lt $record.Count - 1) {
+    $line = $record[$recordIndex++].Trim()
+    if (!$line) { continue }
+    $newRecord.Details += $line
+  }
+
+  # Read last line for End Time
+  $line = $record[$recordIndex].Replace('End Time: ', '').Trim()
+  $newRecord.EndTime = Format-Timestamp -timestamp $line
+
+  if($errorsOnly -and $newRecord.Level -ine 'Error') {
+    return $null
+  }
+
+  if ($quiet -eq $false) {
+    $consoleColor = [System.ConsoleColor]::White
+    switch ($newRecord.Level) {
+      'Error' { $consoleColor = [System.ConsoleColor]::Red }
+      'Warning' { $consoleColor = [System.ConsoleColor]::Yellow }
+      'Information' { $consoleColor = [System.ConsoleColor]::Green }
+    }
+    Write-Host "returning record $($records.Count): $($newRecord | out-string)" -ForegroundColor $consoleColor
+  }
+
+  return $newRecord
+}
+
+function Read-Records($logFilePath) {
+  $streamReader = [System.IO.StreamReader]::new($logFilePath, [System.Text.Encoding]::Unicode)
+  $inRecord = $false
+  $index = 0
+  $record = [collections.arrayList]::new()
+
+  while ($streamReader.EndOfStream -eq $false) {
+    $line = $streamReader.ReadLine().Trim()
+    Write-Verbose "Read-Records: $line"
+    $index++
+
+    if ($line.Length -eq 0) {
+      continue
+    }
+
+    # remove unknown unicode characters outside of ASCII range
+    $line = [string]::Join('', ($line.ToCharArray() | Where-Object { [int]$psitem -ge 32 -and [int]$psitem -le 126 }))
+
+    if ($line.StartsWith($startOfRecord) -and !$inRecord) {
+      # start new record
+      $inRecord = $true
+      $line = $line.Replace($recordIdentifier, '')
+      [void]$record.Add($line)
+    }
+    elseif ($line.StartsWith($endOfRecord) -and $inRecord) {
+      $inRecord = $false
+      $line = $line.Replace($recordIdentifier, '')
+      # add record to results
+      [void]$record.Add($line)
+      [void]$records.Add((Read-Record $record $index))
+      [void]$record.Clear()
+      continue
+    }
+    elseif ($line -eq $recordSeparator) {
+      continue
+    }
+    elseif ($inRecord) {
+      [void]$record.Add($line)
+    }
+    else {
+      Write-Warning "Unknown Record log file format index: $index line: $line"
+    }
+  }
+  return $records
+}
+
+main
